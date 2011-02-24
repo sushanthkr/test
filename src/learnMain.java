fe3a81e20783671e50893563b872f109a9830905
sushanthkr <sushanthkr@gmail.com>
2011-02-23 20:49:49 -0800

    Edited src/learnMain.java via GitHub

--- src/learnMain.java
+++ src/learnMain.java
@@ -1,4 +1,3 @@
-
 public class learnMain {
 
 	/**
@@ -7,7 +6,7 @@ public class learnMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
-		System.out.println("Hello World1");
+		System.out.println("Hello World2");
 	}
 
 }
